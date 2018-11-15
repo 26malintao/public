433bebd09e58ccf8fc21dba10babd07dd8e09aa0
26malintao <343508649@qq.com>
2018-11-15 20:19:36 +0800

    第一个项目

--- src/main/java/com/zl/dao/IUsersDao.java
+++
@@ -1,7 +0,0 @@
-package com.zl.dao;
-
-import com.zl.pojo.User;
-
-public interface IUsersDao {
-	public User IsLogin(User user);
-}
--- src/main/java/com/zl/service/IUsersService.java
+++
@@ -1,7 +0,0 @@
-package com.zl.service;
-
-import com.zl.pojo.User;
-
-public interface IUsersService {
-	public User IsLogin(User user);
-}
